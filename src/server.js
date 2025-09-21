812fd82090e69b6a67f2d3a7c6b933b70e255187
Daraban Andrei <andrei.daraban70@yahoo.com>
2025-09-22 01:21:44 +0300

    update url

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
     "http://localhost:5173", 
-    "https://forum-frontend-three.vercel.app/"
+    "https://forum-frontend-three.vercel.app"
   ],
     credentials: true,
 }));
